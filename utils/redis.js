a1d18acde3c5b4fe007d1bfaf58f016d1687a862
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-06-21 21:13:37 +0200

    feat: implement RedisClient in utils/redis.js
    - Created RedisClient class for managing Redis connections.
    - Methods implemented: isAlive, get, set (with expiration), del.
    - Exported singleton instance redisClient.
    - Task covered: 0. Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+// utils/redis.js
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.error(`Redis Client Error: ${err}`));
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setexAsync = promisify(this.client.setex).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    return this.setexAsync(key, duration, value);
+  }
+
+  async del(key) {
+    return this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
